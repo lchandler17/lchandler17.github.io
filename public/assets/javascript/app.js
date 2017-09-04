90ed5331fcd6a80155cd9422084501082bd778c7
Lauren Chandler <lac104@case.edu>
2017-09-04 11:20:53 -0400

    progress push

--- public/assets/javascript/app.js
+++ public/assets/javascript/app.js
@@ -2,4 +2,17 @@ $(document).ready(function(){
 
 	$('#main').scrollspy({ target: '#nav' });
 
+	//MODAL FUNCTIONS
+
+	$('#mailModal').on('shown.bs.modal', function () {
+  		$('#myInput').focus();
+  		//??? my Input?? //
+	});
+
+	$("#mailSend").on("click", function() {
+
+	});
+
+	$('#mailCancel').modal('hide');
+
 });
